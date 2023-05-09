d94fc245ae5c737846757a502d623959d4a2b5d6
Jorge Andres Sanchez Barreto <jorge.sb@globant.com>
2023-05-09 13:52:25 -0500

    DOCS: Creando clase user

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-import ui.UIMenu;
-
-import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Date;
 
 import static ui.UIMenu.showMenu;
-
 public class Main {
     public static void main(String[] args) {
 
         //showMenu();
 
         Doctor myDoctor = new Doctor("Anahi Salgado", "pediatria");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
         myDoctor.showDoctor();
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()){
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
 
         Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("1234567891");
+        System.out.println(patient.getPhoneNumber());
     }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -14,4 +14,74 @@ public class Patient {
         this.weight = 54.5;
         System.out.println("Peso: " + weight + " Kg.");
     }
+
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+
+    public String getWeight(){
+        return this.weight + "Kg.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if(phoneNumber.length() > 10){
+            System.out.println("El numero telefonico debe ser de 10 digitos maximo");
+        }else if(phoneNumber.length() == 10){
+            this.phoneNumber = phoneNumber;
+        }
+
+
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getHeight() {
+        return height + " Mts.";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
 }
